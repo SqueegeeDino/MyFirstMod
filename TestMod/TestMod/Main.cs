24e1eda1de59fd527a157fb7692b64a648d21c24
SqueegeeDinoToy <65366947+SqueegeeDino@users.noreply.github.com>
2020-08-11 11:26:48 -0700

    Trying to get a second class working, with keypress debugging

--- TestMod/TestMod/Main.cs
+++ TestMod/TestMod/Main.cs
@@ -1,19 +1,21 @@
-﻿using UnityEngine;
-using UnityModManagerNet;
+﻿using UnityModManagerNet;
+using UnityEngine;
 
 namespace TestMod
 {
     static class Main
     {
-        static bool Load(UnityModManager.ModEntry modEntry)
+        // Simply call. Compiled without dependencies on UnityModManagerNet
+        static void Load()
         {
-            modEntry.OnGUI = OnGUI;
-            return true;
+            // Something
         }
 
-        static void OnGUI(UnityModManager.ModEntry modEntry)
+        // Send a response to the mod manager about the launch status, success or not.
+        static bool Load(UnityModManager.ModEntry modEntry)
         {
-
+            UnityModManager.Logger.Log("TestMod - Squeegee Sucks");
+            return true; // If false the mod will show an error.
         }
     }
 }
---
+++ TestMod/TestMod/TestManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityModManagerNet;
+
+namespace TestMod
+{
+
+    public class KeyCodeInput
+    {
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                UnityModManager.Logger.Log("TestMod - Space key was pressed.");
+            }
+
+            if (Input.GetKeyUp(KeyCode.Space))
+            {
+                UnityModManager.Logger.Log("TestMod - Space key was released.");
+            }
+        }
+
+        void Start()
+        {
+            UnityModManager.Logger.Log("TestMod - Update Working");
+        }
+    }
+}

a7d42a5835bfe24c20b1021aa8d9e2ea7839ffb8
SqueegeeDinoToy <65366947+SqueegeeDino@users.noreply.github.com>
2020-08-11 07:46:51 -0700

    Tried to create a release, it isn't working yet :dizzy_face:

---
+++ TestMod/TestMod/Main.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityModManagerNet;
+
+namespace TestMod
+{
+    static class Main
+    {
+        public static bool enabled;
+        public static string health;
+        public static string ammo;
+
+        static bool Load(UnityModManager.ModEntry modEntry)
+        {
+            modEntry.OnGUI = OnGUI;
+            return true;
+        }
+
+        static void OnGUI(UnityModManager.ModEntry modEntry)
+        {
+
+        }
+    }
+}
